46b13157403f046e69de296bf47e978ab0c47404
ksg9482 <ksg930523@gmail.com>
2022-01-30 00:39:33 +0900

    add forders

--- refactoring/server/index.js
+++ refactoring/server/index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 
+const userRouter = require('./routers/user')
+
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
@@ -8,6 +10,9 @@ const app = express();
 app.get('/', (req, res)=>{
     res.send('success');
 });
+
+app.use('/user', userRouter);
+
 
 app.listen(PORT, HOST)
 console.log(`http://${HOST}:${PORT}`)
