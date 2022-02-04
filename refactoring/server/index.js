0bee553422331ac5ed80c8b8f03d91f43502f85d
ksg9482 <ksg930523@gmail.com>
2022-02-04 23:53:16 +0900

    add

--- refactoring/server/index.js
+++ refactoring/server/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 
+
+
 const userRouter = require('./routers/user')
+const postRouter = require('./routers/post')
 
 const PORT = 8080;
 const HOST = "0.0.0.0";
@@ -8,11 +11,11 @@ const HOST = "0.0.0.0";
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('success');
+    res.send('success')
 });
 
-app.use('/user', userRouter);
-
+app.use('/user', userRouter)
+app.use('/post', postRouter)
 
 app.listen(PORT, HOST)
 console.log(`http://${HOST}:${PORT}`)
--- refactoring/server/migrations/20220203140847-create-post.js
+++ refactoring/server/migrations/20220203140847-create-post.js
@@ -9,12 +9,15 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       content: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       userId: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       public: {
+        allowNull: false,
         type: Sequelize.BOOLEAN
       },
       createdAt: {
@@ -25,9 +28,31 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
+    /*.then(() => {
+      queryInterface.addColumn('post', 'userId', {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {model: 'user', key:'id'}
+      })
+    })
+    .then(() => {
+      queryInterface.addColumn('post_tag', 'postId', {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {model: 'post', key:'id'}
+      })
+    }) */
+    ;//왜 then으로 연결했는가? 외부키 설정하면 생기는 건가?
   },
   async down(queryInterface, Sequelize) {
+    /* let sql ='SET FOREIGN_KEY_CHECKS = 0';
+    await queryInterface.sequelize.query(sql, {
+        type: Sequelize.QueryTypes.RAW,
+    })
+    await queryInterface.dropTable('post');  */
     await queryInterface.dropTable('posts');
   }
 };
