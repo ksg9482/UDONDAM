88ca67fea9cc99fe4dcf4e261cace8a11fe4106a
ksg9482 <ksg930523@gmail.com>
2022-04-27 23:11:38 +0900

    delete client folder

--- client/src/components/utils/LoadingIndicator.tsx
+++
@@ -1,5 +0,0 @@
-function LoadingIndicator() {
-    return <img  className="loading-indicator" alt="now loading..." src="loading.gif" style={{ margin: '1rem', width: 'auto', maxHeight: '100vh', maxWidth:'100vw' }} />
-  }
-  
-  export default LoadingIndicator
