9739348c40fd4ff505b3c55d8ec390b01a0b9ca2
ksg9482 <ksg930523@gmail.com>
2022-04-27 22:14:50 +0900

    delete refactoring folder

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME ,
+    username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME || "UDONDAM",
     host: process.env.DATABASE_HOST || "127.0.0.1",
