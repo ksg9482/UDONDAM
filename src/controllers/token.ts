80e4f5672ce39a015a129a93c357ae919d2845c5
ksg9482 <ksg930523@gmail.com>
2022-05-03 15:00:57 +0900

    수정 - 타입스크립트 적용

--- src/controllers/token.ts
+++ src/controllers/token.ts
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { sign } = require('jsonwebtoken');
+const { sign, verify } = require('jsonwebtoken');
 const DOMAIN = process.env.DOMAIN || 'localhost'
 
 module.exports = {
